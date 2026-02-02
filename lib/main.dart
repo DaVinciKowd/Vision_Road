f34ee13426d956d01cbd7a74ff66ddcd84ef01c9
Kristine Suelto <kristinealcantarasuelto@gmail.com>
2026-02-02 18:29:08 +0800

    Updated main app entry and dependencies

--- lib/main.dart
+++ lib/main.dart
@@ -16,3 +16,28 @@ class VisionRoadApp extends StatelessWidget {
     );
   }
 }
+
+
+
+// temporary page
+/*
+import 'package:flutter/material.dart';
+import 'screens/popup_preview_page.dart'; // Import your preview page
+
+void main() {
+  runApp(const VisionRoadApp());
+}
+
+class VisionRoadApp extends StatelessWidget {
+  const VisionRoadApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: PopupPreviewPage(), // TEMPORARY preview
+    );
+  }
+}
+*/
+
