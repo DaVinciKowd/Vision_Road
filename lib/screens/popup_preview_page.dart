4c0af7cedfb09be4c31ea2a6e38c8bf78d286238
Kristine Suelto <kristinealcantarasuelto@gmail.com>
2026-02-02 18:43:32 +0800

    Added a temporary popup preview page for banners

---
+++ lib/screens/popup_preview_page.dart
@@ -0,0 +1,57 @@
+/*
+import 'package:flutter/material.dart';
+import '../widgets/hazard_detected.dart';
+
+class PopupPreviewPage extends StatelessWidget {
+  const PopupPreviewPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey.shade300, // helps see opacity & shadow
+      body: Center(
+        child: HazardDetectedBanner(), // your popup widget
+      ),
+    );
+  }
+}
+*/
+
+// Hazard Registered
+/*
+import 'package:flutter/material.dart';
+import '../widgets/hazard_registered.dart';
+
+class PopupPreviewPage extends StatelessWidget {
+  const PopupPreviewPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey.shade300, // helps see opacity & shadow
+      body: Center(
+        child: HazardRegisteredBanner(), // your popup widget
+      ),
+    );
+  }
+}
+*/
+
+// Hazard ALert
+
+import 'package:flutter/material.dart';
+import '../widgets/hazard_alert.dart';
+
+class PopupPreviewPage extends StatelessWidget {
+  const PopupPreviewPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey.shade300, // helps see opacity & shadow
+      body: Center(
+        child: HazardAlertBanner(), // your popup widget
+      ),
+    );
+  }
+}
